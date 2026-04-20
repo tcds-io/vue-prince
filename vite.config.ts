bc7f221fa515397e80fccefd0f1e2c7fea7f879b
Thiago Cordeiro <thiago@vespertool.com>
2026-04-20 18:51:38 +0200

    fix style

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import cssInjectedByJs from 'vite-plugin-css-injected-by-js'
 import { resolve } from 'node:path'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), cssInjectedByJs()],
   build: {
     lib: {
       entry: resolve(import.meta.dirname, 'src/index.ts'),
